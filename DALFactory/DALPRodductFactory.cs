2d0e0e8a4b14a751c9538d313fe5b01fa52deee9
emilio barrera <barrera_emilio@hotmail.com>
2018-08-20 18:27:39 +0100

    utilizando un app.config y assemblys

--- DALFactory/DALPRodductFactory.cs
+++ DALFactory/DALPRodductFactory.cs
@@ -8,10 +8,31 @@
     using System.Threading.Tasks;
     public class DALPRodductFactory
     {
+        public ProductInterface.IProduct GetDALProduct()
+        {
+            ProductInterface.IProduct product = null;
+              //ruta del assemble desde el appConfig
+            var assemblyPath = System.Configuration.ConfigurationManager.AppSettings["Product"];
+            //aqui cargo el assemble en memoria
+            var assemblyObject = System.Reflection.Assembly.LoadFrom(assemblyPath);
+            var productType = assemblyObject.GetTypes().Where(T =>typeof(ProductInterface.IProduct)
+                .IsAssignableFrom(T)).FirstOrDefault();
+
+            if (productType != null)
+            {
+              product =  assemblyObject.CreateInstance(productType.FullName) as ProductInterface.IProduct;
+            }
+
+
+            return product;
+        }
+
         //public ProductInterface.IProduct GetDALProduct() => new DALJson.Product();
-        //public ProductInterface.IProduct GetDALProduct() => new DALMSQL.Product();
+       // public ProductInterface.IProduct GetDALProduct() => new DALMSQL.Product();
       // public ProductInterface.IProduct GetDALProduct() => new DALExtra.Products();
-        public ProductInterface.IProduct GetDALProduct() => new DAL.Product();
+       // public ProductInterface.IProduct GetDALProduct() => new DAL.Product();
+
+           
 
 
         public string GetIdentity(ProductInterface.IProduct product)
